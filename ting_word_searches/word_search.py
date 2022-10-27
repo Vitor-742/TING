e452ee3db0ff2dfb6350ef971374f0f66b4bd96d
Vitor Campos <vitorcampos742@gmail.com>
2022-10-27 15:48:55 -0300

    requisito 6

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,17 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    aux_list = []
+    for i in range(len(instance)):
+        aux_list.append({
+            "palavra": word,
+            "arquivo": instance.search(i)['nome_do_arquivo'],
+            "ocorrencias": []
+        })
+        for ind, line in enumerate(instance.search(i)['linhas_do_arquivo']):
+            if word.lower() in line.lower():
+                aux_list[-1]['ocorrencias'].append({"linha": ind + 1})
+        if len(aux_list[-1]['ocorrencias']) == 0:
+            aux_list.pop()
+    return aux_list
 
 
 def search_by_word(word, instance):
