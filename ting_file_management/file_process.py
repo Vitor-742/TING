834e9360810f92461063f123d73f52936d067db5
Vitor Campos <vitorcampos742@gmail.com>
2022-10-27 15:11:17 -0300

    requisito 5

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -25,4 +25,8 @@ def remove(instance):
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    if len(instance) <= position:
+        sys.stderr.write('Posição inválida\n')
+        return
+    res = instance.search(position)
+    sys.stdout.write(str(res))
